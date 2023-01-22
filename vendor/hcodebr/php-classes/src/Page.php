6fd8c63a32a7ea20212f035be39a28a7d8906e26
Hugo <hugosantosadm125@gmail.com>
2023-01-22 17:05:27 -0300

    Criamos a classe Page

---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,100 @@
+<?php
+//Criando uma class Page:
+
+//Namespace da pasta Hcode:
+namespace Hcode;
+
+//Utilizando do namespace do Rian Tpl:
+use Rain\Tpl;
+
+//Inicio: Criando a class Page:
+class Page {
+
+	//Atributos:
+	private $tpl;
+	private $options = [];
+	private $defaults = [
+		"data"=>[]
+	];
+
+
+	//Métodos:
+
+	 //Inicio: Método magico construtor:
+	public function __construct($opts = array()) {
+
+		//Realizando o merge dos arrays:
+		$this->options = array_merge($this->defaults, $opts);
+
+		// config
+		$config = array(
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]. "/views/",
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]. "/views-cache/",
+			"debug"         => false // set to false to improve the speed
+		);
+
+		Tpl::configure( $config );
+
+		//Criando uma objeto Tpl:
+		$this->tpl = new Tpl();
+
+		$this->setData($this->options["data"]);
+
+		$this->tpl->draw("header");
+
+
+
+	}
+	 //fim: Método magico construtor:
+
+
+	 //Inicio: Método setData:
+	private function setData($data = array()){
+
+		foreach($data as $key => $value){
+
+			$this->tpl->assign($key,$value);
+
+		}		
+
+	}
+	 //fim: Método setData:
+
+
+	 //Inicio: Método setTpl:
+	public function setTpl($name, $data = array(), $returnHTML = false){
+
+		$this->setData($data);
+
+		//Carregar o template na tela:
+		return $this->tpl->draw($name, $returnHTML);
+
+
+
+	}
+
+
+	 //Fim: Método setTpl:
+
+
+
+	 //Inicio:Método magico destruct:
+
+	public function __destruct() {
+
+		$this->tpl->draw("footer");
+
+	 
+	}
+	 //Fim: Método magico destruct:
+
+
+}
+
+//Fim: Criando a class Page:
+
+
+
+
+
+?>
