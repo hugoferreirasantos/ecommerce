02b40077da8872d10cf7db4e5402bef1cc9a80f6
Hugo <hugosantosadm125@gmail.com>
2023-01-28 18:51:38 -0300

    Criação do CRUD na parte de logins

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $app->get('/', function() {
 //Rota da pagina do admin:
 $app->get('/admin',function(){
 
-	//Método estatico responsavel por verificar o usuário:
+	//Método estatico responsavel por verificar o usuário esta logado:
 	User::verifyLogin();
 
 	$page = new PageAdmin();
@@ -54,6 +54,10 @@ $app->post('/admin/login', function(){
 	header("Location: /admin");
 	exit;
 
+	//echo password_hash('admin',PASSWORD_DEFAULT);
+
+
+
 });
 
 //Rota para o logout:
@@ -65,6 +69,126 @@ $app->get('/admin/logout', function(){
 	exit;
 
 });
+
+//Rota  tela que vai listar todos os usuários:
+$app->get('/admin/users', function(){
+
+	//Método estatico responsavel por verificar o usuário esta logado:
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+
+});
+
+ //incio: CRUD:
+
+//Rota para criar usuários:
+$app->get('/admin/users/create', function(){
+
+	//Método estatico responsavel por verificar o usuário esta logado:
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+
+});
+
+//Rota para exclusão de dados no banco:
+$app->get("/admin/users/:iduser/delete", function($iduser){
+
+	//Método estatico responsavel por verificar o usuário esta logado:
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;	
+
+});
+
+//Rota para dar um update:
+$app->get("/admin/users/:iduser", function($iduser){
+
+	//Método estatico responsavel por verificar o usuário esta logado:
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update", array(
+		"user"=>$user->getValues()
+	));
+
+});
+
+
+
+//Rota para salvar os dados alterados para o banco de dados:
+$app->post("/admin/users/create",function(){
+
+	//Método estatico responsavel por verificar o usuário esta logado:
+	User::verifyLogin();
+
+	//var_dump($_POST);
+
+	//Criar um usuário novo no banco de dados:
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	//var_dump($user);
+
+	//Retornar para a tela de usuarios cadastrados:
+	header("Location: /admin/users");
+	exit;
+
+
+
+});
+
+//Rota para salvar a edição de usuário: UPDATE:
+$app->post("/admin/users/:iduser", function($iduser){
+
+	//Método estatico responsavel por verificar o usuário esta logado:
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
+
+
+});
+
+
+ //fim: CRUD
+
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -97,6 +97,97 @@ class User extends Model {
 
 	 //Fim: Método logout:
 
+	 //Inicio: Método listAll():
+	public static function listAll()
+	{
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
+	}
+
+	 //Fim:: Método listAll():
+
+	 //Inicio: Método save:
+	public function save()
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_users_save(:desperson,:deslogin,:despassword,:desemail,:nrphone,:inadmin)",
+			array(
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results[0]);
+
+
+	}
+	 //Fim: Método save:
+
+	 //Inicio: Método get():
+	public function get($iduser)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", 
+			array(
+				"iduser"=>$iduser
+		));
+
+		$this->setData($results[0]);
+
+	}
+
+	 //Fim: Método get();
+
+	 //Inicio: Método update():
+	public function update()
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser,:desperson,:deslogin,:despassword,:desemail,:nrphone,:inadmin)",
+			array(
+			":iduser"=>$this->getiduser(),
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results[0]);
+
+	}
+
+	 //Fim: Método update():
+
+	 //Inicio: Método delete():
+
+	public function delete()
+	{
+
+
+		$sql = new Sql();
+
+		$sql->query("CALL sp_users_delete(:iduser)",array(
+			":iduser"=>$this->getiduser()
+		));
+
+
+	}
+
+	 //Fim: Método delete():
+
 }
 
 
