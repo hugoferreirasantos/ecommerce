3cb5661ee2da79ff8ea15fadb4346f8d70e48ce5
Hugo <hugosantosadm125@gmail.com>
2023-02-18 17:36:16 -0300

    Site - Categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -39,6 +39,8 @@ class Category extends Model {
 
 		$this->setData($results[0]);
 
+		Category::updateFile();
+
 	}
 
 	//Fim: Método Save();
@@ -69,9 +71,30 @@ class Category extends Model {
 			":idcategory"=>$this->getidcategory()
 		));
 
+		Category::updateFile();
+
 	}
 
 	//Fim: Método Delete()
+
+	//Inicio: Método para atualizar o arquivo:
+	public function updateFile()
+	{
+		//Pegando tudo que esta no banco de dados das categorias:
+		$categories = Category::listAll();
+
+		//Incrementando dinâmicamente o array:
+		$html = [];
+
+		foreach($categories as $row){
+			array_push($html,'<li><a href="/categories/'.$row["idcategory"].'">'.$row["descategory"].'</a></li>');
+		}
+
+		file_put_contents($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR."categories-menu.html", implode('', $html));
+
+	}
+
+	//Fim: Método para atualizar o arquivo:
 
 
 
